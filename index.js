dc046e63006f5d478454b518cba143a20b6f3c02
Phil Birnie <phil@7thstreetweb.com>
2019-08-12 13:21:05 -0400

    Adds Parsing of image urls

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 var postcss = require('postcss')
 
 module.exports = postcss.plugin('postcss-terminal4-media', function (opts) {
-  opts = opts || {}
+  opts = opts || {
+    map: {
+      images: {}
+    }
+  }
+
+  if (!opts.media_string) {
+    opts.media_string = '<t4 type="media" formatter="path/*" id="{d}" />'
+  }
 
   // Work with options here
 
-  return function (root, result) {
-
-    // Transform CSS AST here
-
+  return function (root) {
+    Object.keys(opts.map.images).forEach(function (imageUrl) {
+      root.replaceValues(
+        imageUrl, {
+          props: ['background_image', 'background']
+        }, function () {
+          return opts.media_string.replace('{d}', opts.map.images[imageUrl])
+        })
+    })
   }
 })
--- index.test.js
+++ index.test.js
@@ -2,6 +2,23 @@ var postcss = require('postcss')
 
 var plugin = require('./')
 
+var sampleOpts = {
+  map: {
+    images: {
+      '../images/bg-students.jpg': 834055,
+      '../images/bg-people.png': 834064,
+      '../images/bg-triangle-dots.svg': 834068,
+      '../images/bg-triangle-dots-l.svg': 834069,
+      '../images/arrow-left.svg': 834044,
+      '../images/arrow-right.svg': 834070,
+      '../images/uncommon-good.svg': 834073,
+      '../images/bg-splash.jpg': 834074,
+      '../images/check.svg': 834078
+    }
+  },
+  media_string: '<t4 id="{d}" />'
+}
+
 function run (input, output, opts) {
   return postcss([plugin(opts)]).process(input).then(function (result) {
     expect(result.css).toEqual(output)
@@ -9,10 +26,18 @@ function run (input, output, opts) {
   })
 }
 
-/* Write tests here
-
-it('does something', function () {
-  return run('a{ }', 'a{ }', { })
+it('changes an image to its appropriate map if found', function () {
+  return run(
+    'a{ background_image: url(../images/bg-students.jpg) }',
+    'a{ background_image: url(<t4 id="834055" />) }',
+    sampleOpts
+  )
 })
 
-*/
+it('does not change images if map is not found', function () {
+  return run(
+    'a{background_image: url(../images/dont-change.jpg) }',
+    'a{background_image: url(../images/dont-change.jpg) }',
+    sampleOpts
+  )
+})
